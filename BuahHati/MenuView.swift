f23136183a20814f875feb2582dc2a70aa340add
Sumantap311004 <155468042+Sumantap311004@users.noreply.github.com>
2025-06-09 10:50:05 +0700

    feat: Create MenuView

---
+++ BuahHati/MenuView.swift
@@ -0,0 +1,45 @@
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        VStack(spacing: 40) {
+            Spacer()
+            
+            Image("Logo")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 400, height: 400)
+            
+            // Judul
+            Text("“PoC Buah Hati”")
+                .font(.largeTitle)
+                .multilineTextAlignment(.center)
+                .padding(.horizontal)
+
+            // Tombol Scan
+            Button(action: {
+                // Aksi saat tombol ditekan
+                print("Scan button tapped!")
+            }) {
+                HStack {
+                    Text("Scan Here")
+                        .fontWeight(.medium)
+                    Image(systemName: "camera") // Ikon kamera
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+                .padding(.horizontal, 40)
+            }
+
+            Spacer()
+        }
+        .background(Color.white.edgesIgnoringSafeArea(.all))
+    }
+}
+
+#Preview {
+    MenuView()
+}
