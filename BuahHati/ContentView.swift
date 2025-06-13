96788988b679386d90b7a140dc3df983ff3bdacc
Sumantap311004 <155468042+Sumantap311004@users.noreply.github.com>
2025-06-13 08:25:05 +0700

    ResultView dan ExternalShape

--- BuahHati/ContentView.swift
+++ BuahHati/ContentView.swift
@@ -9,22 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        TabView {
-            HomeView()
-                .tabItem {
-                    Label("Home", systemImage: "house")
-                }
-            
-            RecipesView()
-                .tabItem {
-                    Label("Recipes", systemImage: "book")
-                }
-            
-            StorageInstructionView()
-                .tabItem {
-                    Label("How 2 Store", systemImage: "archivebox")
-                }
-        }
+        ResultView()
     }
 }
 
